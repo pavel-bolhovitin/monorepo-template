0f06c06a6df41eeb1b328e7e07d1e5214fb28356
Pavels Bolhovitins <100925110+pavel-bolhovitin@users.noreply.github.com>
2026-03-01 13:53:23 +0200

    tailwind fixes

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,3 +1,3 @@
-import postcssConfig from "@repo/tailwind-config/postcss";
+import postcssConfig from "@repo/tailwind-config/postcssConfig";
 
 export default postcssConfig;
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeColorsDemo } from "@/components/ThemeColorsDemo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,7 +47,6 @@ export default function Home() {
             center.
           </p>
         </div>
-        <ThemeColorsDemo />
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
